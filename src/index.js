605d94fda05a3a6f7a413f137b8ce7f6554af1aa
kunaldec022002 <kunalingale72@gmail.com>
2023-10-02 13:34:01 +0530

    add document css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import girl01 from './girl01.jpg';
+
+const greetingStyle =
+{
+   display:'inline-block',
+   backgroundColor:'tomato',
+   borderRadius:'5px',
+   boxShadow : '5px 2px 3px 2px black',
+   margin :'10px',
+   padding:'10px',
+   textAlign:'center',
+   marginLeft:'120px',
+   
+}
+
+function Greeting({name,city})
+{
+  return(
+    <div style={greetingStyle}>
+      <img src={girl01}  height="250px" width="250"/>
+      <h1>Hello, {name}</h1>
+      <h1>City:-{city}</h1>
+      <h3>MCA Student at G.H. Raisoni,College</h3>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+  <Greeting name="I am Kajol" city="Amaravti"/>
+  <Greeting name="I am Sakshi" city="Nagpur"/>
+  <Greeting name="I am Karuna" city="Yavtmal"/>
+
+  </>
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
